7f83d3c00f2a993e1ecff42c0c76338cbccf1638
Graham <graham.lim88@gmail.com>
2022-04-17 14:44:55 +0800

    a little work done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 
+const secretWord = ['b', 'a', 'n', 'a', 'n', 'a'];
+
+// the array begins with only _
+
+function Form() {
+  // Control the form input value using a state variable name.
+  const [guessedLetters, setGuessedLetters] = useState([
+    '_',
+    '_',
+    '_',
+    '_',
+    '_',
+    '_',
+  ]);
+
+  const handleChange = (event) => {
+    // Retrieve input field value from JS event object.
+    const inputtedLetter = event.target.value;
+    // Log input field value to verify what we typed.
+    console.log(inputtedLetter);
+    // Set the value stored in component state to be the new value.
+    if (secretWord.includes(inputtedLetter)) {
+      setGuessedLetters(inputtedLetter);
+    }
+  };
+
+  // The value of the input field always matches the value in the state
+  // variable name. When the input value changes, handleChange updates
+  // the value in the state.
+  return (
+    <div>
+      <input value={guessedLetters} onChange={handleChange} />
+      <div>
+        Guessed Letters:
+        {' '}
+        {guessedLetters}
+      </div>
+    </div>
+  );
+}
 export default function App() {
-  return <div>This is App.jsx</div>;
+  return (
+    <Form />
+  );
 }
